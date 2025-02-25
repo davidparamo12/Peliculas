f48f9f5801c0f3085b5eed50937886f9cb7eb894
davidparamo12 <159239549+davidparamo12@users.noreply.github.com>
2025-02-25 11:35:41 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Peliculas
-Crear funciones y un modelo de ML para un dataset de películas
+Objetivos del primer trabajo es EXTRAER, TRANSFORMAR Y CARGAR datos que se pueden deployar.
+
+Nombre de archivo= ETL
+Nombre de archivo final= data_csv (con esta tabla obtenemos los datos finales)
+
+EXTRAER:
+
+1. El primer paso fue importar pocas librerias, las suficientes para poder extraer, transformar y cargar.
+
+2. El uso de pandas permite abrir y juntar ambas bases de datos con la funcion merge-
+
+TRANSFORMAR-
+
+3. El primer problema es que la informacion esta anidada- hay diccionarios dentro de las columnas. 
+Se realizan 4 desanidadas de los diccionarios de acuerdo a los parametros de la tarea y que son importantes para 
+poder alcanzar el objetivo de crear un MVP en el que se pueda recomendar peliculas:
+
+    1. Se separa CAST- para obtener el nombre del actor especialmente y elimino el resto de columnas. También quito caracteres inutiles para que el nombre del actor quede "nombre_actor". Al obtener el nombre del actor lo agrego a la data completa... repito este proceso para:
+
+    2. CREW- aca obtengo el dato del director. Se quedan dos columnas- job_crew y name_crew
+
+    3. BELONGS TO COLLECTION- Me quedo solo con el titulo y lo agrego a mi data completa. Agrego el titulo en minuscula para que sea mas facil el uso de operaciones posteriores.
+
+    4. PRODUCTION COMPANY-Me quedo solo con el nombre de la productora- Proceso pedido por la tarea.
+
+Para la separacion de los datos se utilizo de la funcion- str.split(",", expand=True). Esta funcion separa por "," el diccionario pero a la vez
+genera una gran cantidad de columnas adicionales. Se le pusieron un numero y luego se eliminaron estas columnas adicionales.
+
+Al fusionar el data set credits y movies_dataset, la base de datos inicio con 27 columnas. Al terminar este primer proceso de transformacion se crearon las siguientes columnas- name_actor, job_crew, name_crew, name_movie y name_company
+
+![alt text](image.png)
+
+
+4. De acuerdo al repositorio se rellenaron los valores vacios con 0 para las variables "revenue" y "budget".
+
+5. Se eliminan los datos nulos de "release_date", "vote_average" y "vote_count". Esto se debe a que mejoramos el rendimiento de los datos.
+
+6. El siguiente paso es transformar la columna "release date" que es fundamental para crear el MVP ya que requerimos los valores diarios y mensuales. 
+    1. El primer paso fue usar de la funcion pd.to_datetime de pandas que hace que la columna pase de un dtype=object a datetime64[ns].
+    2. Creamos 3 columnas mas para separar por año, mes y dia de la semana. El mes es un entero al igual que dia semana. Por esta razon requerimos de una transformacion para volverlo un string.
+    3. Creamos un diccionario para mapear un numero de acuerdo al string que queremos de acuerdo al mes y al dia de la semana. El diccionario esta en lower case para facilitar las busquedas posteriores.
+    4. Se eliminan las columnas antiguas y solo quedamos con las columnas release_año (int), release_mes(str), release_dia(str).
+
+7. Los datos "revenue" y "budget" se pasan a enteros y al tipo float. Se genera la division entre ambos para crear la variable "return". Luego todos los vacios son rellenados con ceros. 
+
+8. Se realiza el último barrido de datos- se eliminan las columnas que no sean relevantes y todas los datos que esten nulos de las columnas mas relevantes para el MVP- como el titulo, nombre de actor, nombre de director y nombre de compañia. Por ultimo se filtra la base de datos para que solo esté el nombre del director. La decision de borrar los datos que esten vacios y que solo tengamos los datos del director corresponde por dos razones: 
+    1. para que la subida del archivo sea posible a Render. 
+    2. Mantener unos datos alineados a lo que pide el repositorio. En este sentido se mantienen los datos de las variables que mas se pidieron en el repositorio.  Queda una tabla de 2.369 filas x 18 columnas
+
+9. Se procede a crear una tabla nueva en tipo CSV para que de ahi se saque toda la informacion para las funciones.
+
+
+
+
+2 PARTE DEL TRABAJO- CREACION DE LAS FUNCIONES
+
+1. Desde un archivo tipo python, sin Jupyter, iniciamos el trabajo.
+2. Se importan las librerias correspondientes- en especial sobre Fastapi
---
+++ main.py
@@ -0,0 +1,154 @@
+from fastapi import FastAPI
+import pandas as pd
+import numpy as np
+
+df_mes = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_tabla_mes.csv")
+df_dia = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_tabla_dia")
+df_titulo = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_tabla_titulo")
+df_voto_200 = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_tabla_voto")
+df_actor = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_tabla_actor")
+df_director = pd.read_csv(
+    r"C:\Users\param\OneDrive\Escritorio\Programacion\Soy Henry\Proyecto individual 1\DATASETS\df_nombre_director")
+
+app = FastAPI()
+
+# def cantidad_filmaciones_mes( Mes ): Se ingresa un mes en idioma Español. Debe devolver la cantidad de películas que
+# fueron estrenadas en el mes consultado en la totalidad del dataset.
+# Ejemplo de retorno: X cantidad de películas fueron estrenadas en el mes de X
+
+
+def peli_mes(mes: str):
+    mes = mes.lower()
+    tabla_mes = df_mes[df_mes == mes].count()
+    if tabla_mes.empty:
+        return {"error": f"Porfavor digitar correctamente el mes en español---Please write the month in spanish"}
+    return f"{tabla_mes} cantidad de peliculas fueron estrenadas en el mes de {mes}"
+
+
+# API endpoint that calls the function
+@app.get("/peli_mes/{mes}")
+def get_peli_mes(mes: str):
+    message_1 = peli_mes(mes)
+    return {"message": message_1}
+
+
+# def cantidad_filmaciones_dia( Dia ): Se ingresa un día en idioma Español. Debe devolver la cantidad de películas que
+# fueron estrenadas en día consultado en la totalidad del dataset.
+# Ejemplo de retorno: X cantidad de películas fueron estrenadas en los días X
+
+
+def peli_dia(dia: str):
+    dia = dia.lower()
+    tabla_dia = df_dia[df_dia == dia].count()
+    if tabla_dia.empty:
+        return {"error": f"Porfavor digitar correctamente el día en español---Please write the day in spanish"}
+    return f"{tabla_dia} cantidad de peliculas fueron estrenadas en el dia de {dia}"
+
+
+# API endpoint that calls the function
+@app.get("/peli_dia/{dia}")
+def get_peli_mes(dia: str):
+    message_2 = peli_dia(dia)
+    return {"message": message_2}
+
+
+# def score_titulo( titulo_de_la_filmación ): Se ingresa el título de una filmación esperando como respuesta
+# el título, el año de estreno y el score.
+# Ejemplo de retorno: La película X fue estrenada en el año X con un score/popularidad de X
+
+def score(titulo: str):
+    titulo = titulo.lower()
+    tabla_titulo = df_titulo[df_titulo == titulo]
+    if tabla_titulo.empty:
+        return {"error": f"No se encontró ningún titulo con el nombre {titulo}"}
+    voto_prome = tabla_titulo["vote_average"]
+    año = tabla_titulo["release_año"]
+    popularity_1 = tabla_titulo["popularity"]
+    return f"La pelicula {titulo} fue estrenada el año {año} con una popularidad de {popularity_1} y un promedio de votos de {voto_prome}"
+
+# API endpoint that calls the function
+
+
+@app.get("/score/{titulo}")
+def get_score(titulo: str):
+    message_3 = score(titulo)
+    return {"message": message_3}
+
+
+# def votos_titulo( titulo_de_la_filmación ): Se ingresa el título de una filmación esperando como respuesta el título,
+# la cantidad de votos y el valor promedio de las votaciones. La misma variable deberá de contar con al menos 2000 valoraciones, caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, no se devuelve ningun valor.
+# Ejemplo de retorno: La película X fue estrenada en el año X. La misma cuenta con un total de X valoraciones, con un promedio de X
+
+def score_2000(titulo: str):
+    titulo = titulo.lower()
+    tabla_titulo_2000 = df_voto_200[df_voto_200 == titulo]
+    if tabla_titulo_2000.empty:
+        return f"Esta pelicula no se encuentra en nuestra base de datos. Es probable que tenga menos de 2000 valoraciones."
+    votos = tabla_titulo_2000["vote_count"]
+    promedio = tabla_titulo_2000["vote_average"]
+    año = tabla_titulo_2000["release_year"]
+    return f"La pelicula {titulo} tiene {votos} valoraciones y tiene un promedio de {promedio}"
+
+# API endpoint that calls the function
+
+
+@app.get("/score_2000/{titulo}")
+def get_score(titulo: str):
+    message_4 = score_2000(titulo)
+    return {"message": message_4}
+
+# def get_actor( nombre_actor ): Se ingresa el nombre de un actor que se encuentre dentro de un dataset debiendo devolver
+# el éxito del mismo medido a través del retorno. Además, la cantidad de películas que en las que ha participado y el promedio de retorno.
+# La definición no deberá considerar directores.
+# Ejemplo de retorno: El actor X ha participado de X cantidad de filmaciones, el mismo ha conseguido un retorno de X con un promedio
+# de X por filmación
+
+
+def get_actor(actor: str):
+    actor = actor.lower()
+    tabla_actor = df_actor[df_actor["nombre_actor"] == actor]
+    if tabla_actor.empty:
+        return {"error": f"Porfavor digitar correctamente el nombre del actor principal"}
+    numero_peli = len(tabla_actor)
+    suma_actor = tabla_actor["return"].sum()
+    prom_actor = suma_actor/numero_peli
+    return f"El actor {actor} tiene un retorno total de {suma_actor} con un promedio de {prom_actor}"
+
+
+# API endpoint that calls the function
+
+
+@app.get("/get_actor/{actor}")
+def get_score(titulo: str):
+    message_5 = get_actor(titulo)
+    return {"message": message_5}
+
+
+# def get_director( nombre_director ): Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo
+#  devolver el éxito del mismo medido a través del retorno. Además, deberá devolver el nombre de cada película con la fecha de lanzamiento,
+# retorno individual, costo y ganancia de la misma.
+
+
+def get_director(director: str):
+    director = director.lower()
+    tabla_director = df_director[df_director["nombre_director"] == director]
+    if tabla_director.empty:
+        return {"error": f"No se encontró ningún director con el nombre {director}"}
+    numero_peli_dir = len(tabla_director)
+    suma_director = tabla_director["return"].sum()
+    prom_director = suma_director/numero_peli_dir
+    return f"El director {director} tiene un retorno total de {suma_director} con un promedio de {prom_director}"
+
+
+# API endpoint that calls the function
+
+
+@app.get("/get_director/{director}")
+def get_dir(director: str):
+    message_6 = get_director(director)
+    return {"message": message_6}
